9c6d40b8dceb727cd4ce8660a2e2f74a84da598c
ddmitrienko <ddmitrienko@plesk.com>
2021-12-22 18:19:29 +0700

    Task #3 Fibonacci: second take

--- fib_task.php
+++ fib_task.php
@@ -2,21 +2,19 @@
 
 function fib(int $n): int
 {
+    if ($n === 0) {
+        return 0;
+    }
+
+    if ($n === 1) {
+        return 1;
+    }
+
     $result = 0;
-    switch ($n) {
-        case 0:
-            $result = 0;
-            break;
-        case 1:
-            $result = 1;
-            break;
-        default:
-            $array = [0,1];
-            for($i=2; $i<=$n; $i++) {
-                $array[$i] = $array[$i-1] + $array[$i-2];
-                $result = $array[$i];
-            }
-            break;
+    $array = [0, 1];
+    for ($i = 2; $i <= $n; $i++) {
+        $array[$i] = $array[$i - 1] + $array[$i - 2];
+        $result = $array[$i];
     }
     return $result;
 }
