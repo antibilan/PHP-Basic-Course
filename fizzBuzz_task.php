2ed64c8eeb101f5a8943d1c5e492d69d2dc76d87
ddmitrienko <ddmitrienko@plesk.com>
2021-12-23 21:50:02 +0700

    Task #8 fizzBuzz

---
+++ fizzBuzz_task.php
@@ -0,0 +1,24 @@
+<?php
+
+function fizzBuzz2(int $begin, int $end): bool
+{
+    if ($begin > $end) {
+        return false;
+    }
+
+    for ($i = $begin; $i <= $end; $i++) {
+
+        if ($i % 3 === 0) {
+            if ($i % 5 === 0) {
+                print_r("FizzBuzz ");
+            } else {
+                print_r('Fizz ');
+            }
+        } elseif ($i % 5 === 0) {
+            print_r('Buzz ');
+        } else {
+            print_r("$i ");
+        }
+    }
+return true;
+}
