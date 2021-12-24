83cebe690f2464723ce3c6cb9d7ab4255c4edda1
antibilan <ddmitrienko@plesk.com>
2021-12-24 15:39:54 +0700

    Fixup!

--- isPerfect_task.php
+++ isPerfect_task.php
@@ -4,12 +4,12 @@ function isPerfect(int $n): bool
 {
     $dividers = [];
 
-    if($n <= 1) {
+    if ($n <= 1) {
         return false;
     }
 
-    for($i = 1; $i <= $n/2; $i++) {
-        if($n % $i === 0) {
+    for ($i = 1; $i <= $n / 2; $i++) {
+        if ($n % $i === 0) {
             $dividers[] = $i;
         }
     }
