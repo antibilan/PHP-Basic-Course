8c5c310bc170337a3c6e48fa5c10dd37731746c7
antibilan <ddmitrienko@plesk.com>
2021-12-24 15:37:49 +0700

    Task #5 Brackets (isBalanced) (#5)
    * Task #5 Brackets (isBalanced)

---
+++ isBalanced_task.php
@@ -0,0 +1,19 @@
+<?php
+
+function isBalanced(string $string): bool
+{
+    $array = str_split($string);
+    $count = 0;
+
+    foreach ($array as $value) {
+        if ($value === '(') {
+            $count++;
+        } elseif ($value === ')') {
+            if ($count === 0) {
+                return false;
+            }
+            $count--;
+        }
+    }
+    return $count === 0;
+}
