1514a5d29f96925a0898271387855fc475ea25f1
antibilan <ddmitrienko@plesk.com>
2021-12-24 15:55:32 +0700

    Fixup!

--- fizzBuzz_task.php
+++ fizzBuzz_task.php
@@ -1,13 +1,12 @@
 <?php
 
-function fizzBuzz2(int $begin, int $end): bool
+function fizzBuzz(int $begin, int $end): bool
 {
     if ($begin > $end) {
         return false;
     }
 
     for ($i = $begin; $i <= $end; $i++) {
-
         if ($i % 3 === 0) {
             if ($i % 5 === 0) {
                 print_r("FizzBuzz ");
@@ -20,5 +19,5 @@ function fizzBuzz2(int $begin, int $end): bool
             print_r("$i ");
         }
     }
-return true;
+    return true;
 }
